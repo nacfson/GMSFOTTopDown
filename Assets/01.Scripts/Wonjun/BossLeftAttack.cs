34c4432f9e75a7e93abb1ae75efd17531cc4d95b
Wizone06 <haemjjiyul548@gmail.com>
2023-04-06 19:11:51 +0900

    보스 양쪽 공격 트리거
    보스 양쪽 공격 트리거

--- Assets/01.Scripts/Wonjun/BossLeftAttack.cs
+++ Assets/01.Scripts/Wonjun/BossLeftAttack.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class BossLeftAttack : MonoBehaviour
 {
     Boss boss;
+    public LayerMask a;
+    RaycastHit2D hit;
+    public Vector3 dir = new Vector3(4, 4, 0);
     private void Awake()
     {
         boss = FindObjectOfType<Boss>();
     }
+    private void Update()
+    {
+        hit = Physics2D.BoxCast(transform.position, dir, 0, Vector2.down, a);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, dir);
+    }
     public void BossLeftpool()
     {
+        if (hit.collider.CompareTag("Player")){
+            Debug.Log("1´Ù¾Æ¹ö·Ç!");
+        }
         PoolList.instance.Push(boss.b);
         Debug.Log("¿ÞÂÊ");
     }
