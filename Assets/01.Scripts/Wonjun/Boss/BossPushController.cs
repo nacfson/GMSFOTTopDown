ee91fde830680af702ff5b2284f5d5ef9d51e131
Wizone06 <haemjjiyul548@gmail.com>
2023-04-20 20:23:04 +0900

    미는 공격 트리거
    미는 공격 트리거

--- Assets/01.Scripts/Wonjun/Boss/BossPushController.cs
+++ Assets/01.Scripts/Wonjun/Boss/BossPushController.cs
@@ -6,16 +6,29 @@ public class BossPushController : MonoBehaviour
 {
     [SerializeField]
     private float _moveSpeed = 4f;
+    [SerializeField]
+    private LayerMask _layermask;
+    RaycastHit2D hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * _moveSpeed);
+        hit = Physics2D.BoxCast(transform.position, new Vector3(2, 3, 1), 0, Vector2.zero, _layermask);
+
+        if (hit)
+        {
+            Debug.Log("하모예");
+        }
+        else
+        {
+            Debug.Log("안닿았어");
+        }
     }
 
     public void PushObject()
