ba2e65004f7062a2f3b86e7e292f57ce3b741502
Wizone06 <haemjjiyul548@gmail.com>
2023-04-26 19:20:08 +0900

    보스 죽음 보스 죽음 애니메이션
    보스 죽음 보스 죽음 애니메이션

--- Assets/01.Scripts/Wonjun/Boss/BossAttack.cs
+++ Assets/01.Scripts/Wonjun/Boss/BossAttack.cs
@@ -8,10 +8,33 @@ public class BossAttack : MonoBehaviour
     [SerializeField] private GameObject[] prefabs;
     [SerializeField] private float spawnInterval;
     [SerializeField] private float spawnRadius;
+    [SerializeField]
+    private float _bossHp;
+    [SerializeField]
+    private EnemySO bossSO;
+    private Animator _anim;
 
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
     private void Start()
     {
         InvokeRepeating("SpawnBoss", 5.5f, spawnInterval);
+        _bossHp = bossSO.hp;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _bossHp -= 10f;
+            Debug.Log(_bossHp);
+        }
+        else if (_bossHp <= 0)
+        {
+            _anim.SetTrigger("Dead");
+        }
     }
 
     private void SpawnBoss()
@@ -33,5 +56,9 @@ public class BossAttack : MonoBehaviour
         {
             PoolList.instance.Push(boss);
         }
+    }
+    private void BossDead()
+    {
+        Destroy(gameObject);
     }
 }
